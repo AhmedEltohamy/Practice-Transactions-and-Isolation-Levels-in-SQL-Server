b954369c51b708aaea23db01619a965e11d8c530
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-04-27 14:13:30 +0200

    Apply transactions and isolation levels concepts in EF Core 6

---
+++ src/EF Core 6/Transactions/Transactions/Data Access/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Transactions.Entities;
+
+namespace Transactions.Data_Access
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=.;Initial Catalog=Test_Transactions;Integrated Security=True");
+        }
+    }
+}
